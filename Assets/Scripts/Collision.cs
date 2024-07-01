36dcefd23498ba70384c53631d6a6565ab6400f2
srabonx <srabon9012@gmail.com>
2024-07-01 23:03:28 +0600

    Added simple mechanism of picking and dropping packages

--- Assets/Scripts/Collision.cs
+++ Assets/Scripts/Collision.cs
@@ -6,23 +6,38 @@ using UnityEngine;
 public class Collision : MonoBehaviour
 {
      bool m_hasPackage = false;
+
+     [SerializeField]Color32 m_hasPackageColor = new Color32(243,237,0,255);
+
+     [SerializeField]Color32 m_noPackageColor = new Color32(255,255,255,255);
+     [SerializeField] float m_destroyTime = 0.2f;
+
+     SpriteRenderer m_spriteRenderer;
+
+     
+     void Start()
+     {
+          m_spriteRenderer = GetComponent<SpriteRenderer>();
+     }
+
    private void OnCollisionEnter2D(Collision2D other)
    {
-        Debug.Log("Bumped"); 
+     
    }
 
    private void OnTriggerEnter2D(Collider2D other) 
    {
         if(other.tag == "Package" && !m_hasPackage)
         {
-           Debug.Log("Package picked up!");
            m_hasPackage = true;
+           Destroy(other.gameObject, m_destroyTime);
+           m_spriteRenderer.color = m_hasPackageColor;
         }
         else if(other.tag == "DeliverySpot" && m_hasPackage)
         {
-          Debug.Log("Package delivered!");
           m_hasPackage = false;
+           m_spriteRenderer.color = m_noPackageColor;
         }
           
-   }
+   }   
 }
--- Assets/Scripts/Driver.cs
+++ Assets/Scripts/Driver.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
+    [SerializeField] private float m_steerSpeed = 200f;
+    [SerializeField] private float m_moveSpeed = 20f;
+    [SerializeField] private float m_slowSpeed = 10f;
+    [SerializeField] private float m_boostSpeed = 35f;
+
+    private float m_timePassed = 0.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,14 +20,36 @@ public class Driver : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        m_timePassed += Time.deltaTime;
+
         float steerDir = Input.GetAxis("Horizontal") * m_steerSpeed * Time.deltaTime;
         float moveDir = Input.GetAxis("Vertical") * m_moveSpeed * Time.deltaTime;
 
         transform.Rotate(0, 0, -steerDir);
         transform.Translate(0, moveDir, 0);
         
+        if(m_timePassed >= 5.0f)
+        {
+            m_moveSpeed = 20;
+            m_timePassed = 0.0f;
+        }
+
     }
 
-    [SerializeField] private float m_steerSpeed = 200f;
-    [SerializeField] private float m_moveSpeed = 20f;
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        m_moveSpeed = m_slowSpeed;
+        m_timePassed = 0.0f;
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "BoostSpeed")
+        {
+            m_moveSpeed = m_boostSpeed;
+            m_timePassed = 0.0f;
+        }
+    }
+
 }
